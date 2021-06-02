886f18dcc26ab9a0e99f4eccd98312592ad4dfc7
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-02 11:48:44 +0200

    verify exist email, encypt the password and saved to DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,39 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
 const createUser = async (req, res = response) => {
-  const { name, email, password } = req.body;
-  res.json({
-    ok: true,
-    user: 'new',
-    name,
-    email,
-    password,
-  });
+  try {
+    const { email, password } = req.body;
+
+    // verificar que el email no exista
+    const existEmail = await User.findOne({ email });
+    if (existEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Email already exist',
+      });
+    }
+
+    const user = new User(req.body);
+
+    // encriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
+    // grabar usuario en DB
+    await user.save();
+
+    res.json({
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Please be in contact with the administrator',
+    });
+  }
 };
 
 const userLogin = async (req, res) => {
