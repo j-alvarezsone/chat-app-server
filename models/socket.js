043f0439537ba9e0386d9e3330122391a26f98ed
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-08 10:32:59 +0200

    added getUsers to emit users-list

--- models/socket.js
+++ models/socket.js
@@ -1,4 +1,4 @@
-const { userConnected, userDisconnected } = require('../controllers/socket');
+const { userConnected, userDisconnected, getUsers } = require('../controllers/socket');
 const { verifyJWT } = require('../helpers/jwt');
 
 class Sockets {
@@ -22,7 +22,8 @@ class Sockets {
       await userConnected(uid);
       // TODO validar el JWT, si el token no es valido, desconectar
       // TODO saber que usuario esta activo mediante el UID
-      // TODO emitir todos los usuarios conectados
+      // emitir todos los usuarios conectados
+      this.io.emit('users-list', await getUsers());
       // TODO socket join
       // TODO  escuchar cuando el cliente manda un mensaje, mensaje personal
       // TODO  disconnect, marcar en el DB que el usuario se desconecto
