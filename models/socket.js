c358d46068ea40c2e1f44f8abfd67d733051b230
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-06-08 22:49:02 +0200

    join the user to socket.io room and listen socket from frontend

--- models/socket.js
+++ models/socket.js
@@ -20,12 +20,19 @@ class Sockets {
       }
 
       await userConnected(uid);
+
+      // Unir al usuario a una sala de socket.io
+      socket.join(uid);
+
       // TODO validar el JWT, si el token no es valido, desconectar
       // TODO saber que usuario esta activo mediante el UID
       // emitir todos los usuarios conectados
       this.io.emit('users-list', await getUsers());
       // TODO socket join
       // TODO  escuchar cuando el cliente manda un mensaje, mensaje personal
+      socket.on('personal-message', (payload) => {
+        console.log(payload);
+      });
       // TODO  disconnect, marcar en el DB que el usuario se desconecto
       // TODO  emitir todos los usuarios conectados
 
